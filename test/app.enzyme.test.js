6851ff7d5d1444135ef2315373bd9300d063a1d2
Javier Armendariz <jarmendariz@ml-9p1g8wn.beachbody.local>
2017-07-19 16:52:08 -0700

    Changing the Mocha path

--- test/app.enzyme.test.js
+++ test/app.enzyme.test.js
@@ -2,11 +2,15 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
 import App from '../src/containers/app/App'
+import Home from '../src/containers/home/Home';
 
 const wrapper = shallow(<App />);
 
+// console.log('wrapper:', wrapper);
+
 describe('(Component) App', () => {
   it('renders...', () => {
-    expect(wrapper).to.have.length(1);
+    // expect(wrapper).to.have.length(1);
+    expect(wrapper.find(Home));
   });
 });
