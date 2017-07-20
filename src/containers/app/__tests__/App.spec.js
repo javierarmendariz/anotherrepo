ddedcb14e074e07f0c2dd1a15b069f94c832bf72
Javier Armendariz <jarmendariz@ml-9p1g8wn.beachbody.local>
2017-07-19 17:06:04 -0700

    Adding __test__ folder. Changin Test file naming convention.

--- src/containers/app/__tests__/App.spec.js
+++ src/containers/app/__tests__/App.spec.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { expect } from 'chai'
-import App from '../src/containers/app/App'
-import Home from '../src/containers/home/Home';
+import App from '../App.js';
+import Home from '../../home/Home';
 
 const wrapper = shallow(<App />);
-
-// console.log('wrapper:', wrapper);
 
 describe('(Component) App', () => {
   it('renders...', () => {
